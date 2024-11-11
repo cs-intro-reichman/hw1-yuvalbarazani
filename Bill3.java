6dd890d1b5ddafe3550bc8f11ed1dc7619d28369
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-12 01:58:15 +0200

    Update Bill3.java
    Done

--- Bill3.java
+++ Bill3.java
@@ -2,7 +2,12 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+	 	 String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		int billAmount = Integer.parseInt(args[3]);
+		double temp = Math.ceil(billAmount/3.0);
+
+		System.out.println("Dear " + name1 + ", " + name2 + ", and "+ name3+": pay " + temp + " Shekels each.");  
 	}
 }
