03b252f0c36989827bf64e8448a66b732eae489c
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-12 01:57:37 +0200

    Update Ascend.java
    Done

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		int max =Math.max(a, Math.max(b,c));
+		int min =Math.min(a, Math.min(b,c));
+		int middle= (a+b+c)-(max+min);
+
+		System.out.println(min + " " + middle + " " + max );
 	}
 }
