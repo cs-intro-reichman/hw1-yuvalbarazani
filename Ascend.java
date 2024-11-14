1d7505ee71672e6ce0e98b007cf9eac75036fb74
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-14 22:52:20 +0200

    Update Ascend.java

--- Ascend.java
+++ Ascend.java
@@ -3,7 +3,7 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int lim = Integer.parseInt(args[0]);
-		int a = (int)(Math.random() * lim);
+		int a = (int)(Math.random() * lim); // creates random int from 0 to lim (lim not included)
 		int b = (int)(Math.random() * lim);
 		int c = (int)(Math.random() * lim);
 
