1339213b53f7d0210c453e0351c17d893f2752b2
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-14 22:48:21 +0200

    Update Ascend.java

--- Ascend.java
+++ Ascend.java
@@ -3,9 +3,9 @@
 public class Ascend {
 	public static void main(String[] args) {
 		int lim = Integer.parseInt(args[0]);
-		int a = (int)(Math.random() * 100);
-		int b = (int)(Math.random() * 100);
-		int c = (int)(Math.random() * 100);
+		int a = (int)(Math.random() * lim);
+		int b = (int)(Math.random() * lim);
+		int c = (int)(Math.random() * lim);
 
 		int max = Math.max(a, Math.max(b, c));
 		int min = Math.min(a, Math.min(b, c));
