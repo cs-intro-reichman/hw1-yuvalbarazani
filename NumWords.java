9061ca9d8ffaa6a51061ea78e07ed0923d6b7569
yuvalbarazani <yuval.barazani@post.runi.ac.il>
2024-11-14 22:39:33 +0200

    Update NumWords.java
    final

--- NumWords.java
+++ NumWords.java
@@ -3,6 +3,7 @@ public class NumWords {
 	public static void main(String args[]) {
             int num = Integer.parseInt(args[0]);
 
-	    System.out.println((num/100)+" hundreds, " + ((num/10) % 10) + " tens, and " + (num%10) + " ones.");
+	    System.out.println((num / 100) + " hundreds, " + ((num / 10) % 10) +
+			       " tens, and " + (num % 10) + " ones.");
 	}
 }
